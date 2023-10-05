9e36d6bc5d792a7b8a549b4c5411955c3b25bbb7
Sam Scotson <118854225+Sam-Scotson@users.noreply.github.com>
2023-10-05 13:32:10 +1300

    Add files via upload

---
+++ learnBasicsOwnership.rs
@@ -0,0 +1,28 @@
+fn main() {
+    // Create an empty vector to store student names
+    let mut student_names: Vec<String> = Vec::new();
+
+    // Add some student names to the vector
+    let name1 = String::from("Alice");
+    let name2 = String::from("Bob");
+
+    student_names.push(name1);
+    student_names.push(name2);
+
+    // Attempt to access name1 after it has been moved
+    // Uncommenting the following line would result in a compilation error:
+    // println!("Name1: {}", name1);
+
+    // Access and print student names from the vector
+    for name in &student_names {
+        println!("Student Name: {}", name);
+    }
+
+    // Modify a student name
+    let mut third_name = String::from("Charlie");
+    student_names.push(third_name);
+
+    // Attempt to access third_name after it has been moved
+    // Uncommenting the following line would result in a compilation error:
+    // println!("Third Name: {}", third_name);
+}
